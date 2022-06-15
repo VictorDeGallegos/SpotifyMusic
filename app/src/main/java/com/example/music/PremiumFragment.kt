ec753b95e195d83989d3685f9d6b0c32def94662
Victor Gallegos <316160456@ciencias.unam.mx>
2022-06-15 00:37:48 -0500

    Añadiendo datos dinamicos a fragment_premium.xml

--- app/src/main/java/com/example/music/PremiumFragment.kt
+++ app/src/main/java/com/example/music/PremiumFragment.kt
@@ -1,12 +1,62 @@
 package com.example.music
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.music.databinding.FragmentPremiumBinding
+import com.example.music.firebase.user.UserInfo
+import com.example.music.util.Constants
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
-class PremiumFragment : Fragment(R.layout.fragment_premium) {
+class PremiumFragment : Fragment() {
+    private var _binding: FragmentPremiumBinding? = null
 
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPremiumBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val sharedPref = requireActivity().getSharedPreferences(
+            Constants.PREF_KEY, Context.MODE_PRIVATE)
+        val userid = sharedPref.getString(Constants.PREF_USERID, "")
+        val db = Firebase.firestore // inicialización de DB
+        db.collection(Constants.FIRESTORE_KEY_USER).get()
+            .addOnSuccessListener { documentList ->
+                if (documentList != null && !documentList.isEmpty){ //mandando a llamar usuarios
+                    val userList = documentList.toObjects(UserInfo::class.java)
+                    val user = userList.firstOrNull() { it.authId == userid }
+                    showUserData(user)
+                }
+            }
+            .addOnFailureListener { e ->
+                Log.w("Firestore", "Error adding document", e)
+            }
+    }
+
+    private fun showUserData(user: UserInfo?) {
+        user?.let {
+            binding.premiumNombretv.text=it.name
+            binding.premiumEmailtv.text=it.email
+            
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
